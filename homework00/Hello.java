15a10bcbca3a8d73668ae938c4ec77107d65c035
cferrari1 <35469659+cferrari1@users.noreply.github.com>
2018-01-15 14:10:12 -0800

    Initial version of Hello.java

---
+++ homework00/Hello.java
@@ -0,0 +1,22 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Hello.java
+ *  Purpose       :  Says "Hello world"; first program; provides a header template for Java programs
+ *  Author        :  Christopher Ferrari
+ *  Date          :  2018-01-15
+ *  Description   :  See purpose (in this case; other files will have more details)
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  Revision History
+ *  ----------------
+ *            Rev      Date         Modified by:     Reason for change/modification
+ *           -----  ----------  -------------------  -----------------------------------------------------------
+ *  @version 1.0.0  2017-01-15  Christopher Ferrari  Initial writing and release
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
