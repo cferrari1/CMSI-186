5bc9dc6bcab919a04cc77427b4b6f83739f0aa67
cferrari1 <35469659+cferrari1@users.noreply.github.com>
2018-04-26 10:30:06 -0700

    Initial upload

---
+++ homework07/DynamicChangeMaker.java
@@ -0,0 +1,21 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name  :  DynamicChangeMaker.java
+ * Purpose    :  Learning exercise to implement tuples to find the optimal amount of change
+ * @author    :  Christopher Ferrari
+ * Date       :  2018-04-26
+ * Description:  @see <a href='https://bjohnson.lmu.build/cmsi186web/homework07.html'>Assignment Page</a>
+ * Notes      :  None
+ * Warnings   :  None
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * Revision History
+ * ================
+ *   Ver      Date     Modified by:  Reason for change or modification
+ *  -----  ----------  ------------  ---------------------------------------------------------------------
+ *  1.0.0  2017-04-26  C. Ferrari    Initial writing and begin coding
+ *
+ *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+public class DynamicChangeMaker {
+    
+}
